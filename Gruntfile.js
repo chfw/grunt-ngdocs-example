f4b02c914033e3c2b9738b9069df66e5fe5a53f1
chfw <wangc_2011@hotmail.com>
2016-05-19 12:16:05 +0100

    1) include plantuml for generating uml graphs 2) include watch to do live reload 3) include connect:open to open browser automatically 4) copy images automatically 5) grunt serve is added

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,88 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-ngdocs');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-plantuml');
 
   grunt.initConfig({
+    plantuml: {
+          all: {
+              src: ['ngdocs/*.uml'],
+              dest: 'docs/images'
+          }
+    },
+    copy: {
+      images: {
+        files: [
+          {
+            nonull: true,
+            expand: true,
+            cwd: 'ngdocs/images/',
+            dest: 'docs/images/',
+            src: ['*.*']
+          }
+        ]
+      }
+    },
     ngdocs: {
       options: {
         scripts: ['angular.js', '../src.js'],
-        html5Mode: false
+        title: 'Sample Documentation',
+        html5Mode: false,
+        template: 'index.tmpl',
+        startPage: 'guide'  
       },
-      all: ['src/*.js']
+      guide: {
+          src: ['ngdocs/*.ngdoc',
+               ],
+        title: 'Tutorial'
+      },
+      api: {
+        src: [
+          'src/*.js'
+        ],
+        title: 'API documentation'
+      }
     },
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+        plantuml: {
+            files: ['ngdocs/*.uml'],
+            tasks: ['plantuml'],
+            options: {
+                livereload: true
+            }
+        },
+        ngdocs: {
+            files: ['ngdocs/*.ngdoc'],
+            tasks: ['ngdocs'],
+            options: {
+                livereload: true
+            }
+        },
+        images: {
+            files: ['ngdocs/images/*'],
+            tasks: ['copy'],
+            options: {
+                livereload: true
+            }
+        }
+    },  
     connect: {
-      options: {
-        keepalive: true
-      },
-      server: {}
+        server: {
+            options: {
+                base: 'docs',
+                livereload: true,
+                open: true
+            }
+        }
     },
     clean: ['docs']
   });
 
-  grunt.registerTask('default', ['clean', 'ngdocs', 'connect']);
+    grunt.registerTask('default', ['clean', 'copy', 'plantuml:all', 'ngdocs']);
+
+    grunt.registerTask('serve', ['clean', 'copy', 'plantuml:all', 'ngdocs',  'connect:server', 'watch']);
 
 };
--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 * install [grunt-cli](https://github.com/gruntjs/grunt-cli) `npm install -g grunt-cli`
 * Then install dependencies with: `npm install`
 
-## Build and run
+## Build docs
 
-Run `grunt` and open http://localhost:8000/docs in your Browser.
+Run `grunt` and documentaion is in docs folder
+
+## Write docs
+
+Run `grunt serve` and automatically your browser presents your docs.
